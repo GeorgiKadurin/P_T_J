b66b7536fa9ecd98f371d5bb99709162bf976854
GeorgiKadurin <120027713+GeorgiKadurin@users.noreply.github.com>
2023-02-03 14:06:40 +0200

    Update P_T_J.py

--- P_T_J/P_T_J.py
+++ P_T_J/P_T_J.py
@@ -1,6 +1,50 @@
-﻿from math import*
+from math import*
 from random import*
 
+
+
+
+
+
+
+
+while True:
+   ik=input("Anna isikukood: ") #str
+   if len(ik)!=11:   #ne rovno 11
+       print("Liiga palju või liiga vähe sümboleid. Sisesta veel kord: ")
+   else:
+        print("Isikukoodi kontroll")
+        ik_list=list(ik)
+        s1=int(ik_list[0]) #"1"->1
+        if s1 not in [1,2,3,4,5,6]:
+            print("Esimine sümbol ei ole õige!")
+        else:
+            print("Esimine sümbol on õige")
+            y=ik_list[1]+ik_list[2] #aasta
+            m=ik_list[3]+ik_list[4] #kuu
+            d=ik_list[5]+ik_list[6] #päev
+            if (int(m)<1 or int(m)>13) and (int(d)<1 or int(d)>31):
+                print("Sünnipäev ei saa luua")
+            else:
+                if s1==1 or s1==2:
+                    yy="18"
+                elif s1==3 or s1==4:
+                    yy="19"
+                else:
+                    yy="20"
+                spaev=str(d)+"."+str(m)+"."+str(y) #ei ole ´18..,19,
+                print(f"Sünnipäev on {spaev}")
+                print({f"Viimane number: {ik_list[-1]}"})
+            
+
+
+
+            
+
+
+#--------------------------------------
+A= [ 3, 4, -1, 5, 0, 10, -12]
+print([A[3]])
 #Задание 6: Проверка имени
 nfs=input("Введите имя => ")
 if nfs.isalpha():
@@ -22,7 +66,7 @@ if nfs.isalpha():
 else:
     print("Ошибка")
 
-
+#-------------------------
 
 #Задание 4: Сортировка
 spisok=[-45,74,29,-1,0]
@@ -33,6 +77,7 @@ print(spisok)
 
 
 
+#5
 
 
 
